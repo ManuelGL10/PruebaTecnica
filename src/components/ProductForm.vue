1b5f8b7a4d850938235a4dd013c157db53c3c1b4
Luis Manuel Gomez <luismanuelgomezlopez18@gmail.com>
2025-08-09 14:09:03 -0600

    Update ProductForm
    Validación de campos antes de guardar información. (Validar datos correctos, que no se encontraran vacíos, fecha futura)

--- src/components/ProductForm.vue
+++ src/components/ProductForm.vue
@@ -3,30 +3,30 @@
         <v-text-field
           v-model="product.name"
           label="Nombre"
-          :rules="[v => !!v || 'El nombre es obligatorio']"
+          :rules="[rules.required]"
           required
         />
         <v-text-field 
         v-model.number="product.quantity"
         label="Cantidad"
         type="number"
-        :rules="[v => v >= 0 || 'Cantidad no puede ser negativo']"
+        :rules="[rules.required, rules.positive]"
+        required
         />       
         <v-text-field 
         v-model.number="product.price"
         label="Precio"
         type="number"
-        :rules="[v => v >= 0 || 'Precio no puede ser negativo']"
+        :rules="[rules.required, rules.positive]"
         required        
         />
         <v-text-field 
         v-model="product.expiryDate"
         label="Fecha de vencimiento"
         type="date"
-        :rules="[v => !!v || 'Fecha es obligatorio']"
+        :rules="[rules.required, rules.futureDate]"
         required
         />
-
         <v-card-actions class="justify-end">
             <v-btn text @click="$emit('cancel')">Cancelar</v-btn>
             <v-btn color="primary" type="submit">Guardar</v-btn>
@@ -50,8 +50,8 @@ const emit = defineEmits(['save', 'cancel'])
 const product = reactive({
   id: null,
   name: '',
-  quantity: 0,
-  price: 0,
+  quantity: null,
+  price: null,
   expiryDate: ''
 })
 
@@ -59,13 +59,26 @@ watch(() => props.initialProduct, (newVal) => {
   Object.assign(product, newVal)
 }, { immediate: true })
 
+const rules = {
+  required:  v => (v !== null && v !== undefined && String(v).trim() !== '') || 'Este campo es obligatorio',
+  positive: v => (!isNaN(v) && Number(v) > 0) || 'Debes ser un numero positivo', 
+  futureDate: v => {
+    if (!v) return 'Este campos es obligatorio'
+    return new Date(v) > new Date() || 'La fecha debe ser futura'
+  }
+}
+
 const form = ref(null)
 
-function submit() {
-  if (!product.id) {
+async function submit() {
+ const {valid} = await form.value.validate()
+  if(!valid){
+  return
+ }
+  if(!product.id){
     product.id = Date.now()
   }
-  emit('save', { ...product }) 
+  emit('save', {...product})
 }
 
 </script>
